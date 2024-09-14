72959fcd733a53d196246e12d785aee3a5731999
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-14 12:13:59 +0530

    adding getLeaveById mapping in Leave controller

--- src/main/java/com/CodingNinjas/LeaveXpress/controller/LeaveController.java
+++ src/main/java/com/CodingNinjas/LeaveXpress/controller/LeaveController.java
@@ -1,10 +1,26 @@
 package com.CodingNinjas.LeaveXpress.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.CodingNinjas.LeaveXpress.model.LeaveModel;
+import com.CodingNinjas.LeaveXpress.service.LeaveService;
 
 @RestController
 @RequestMapping("/api/leave")
 public class LeaveController {
     
+    @Autowired
+    private LeaveService leaveService;
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public LeaveModel getLeaveById(@PathVariable Long id){
+        return leaveService.getLeaveById(id);
+    }
 }
