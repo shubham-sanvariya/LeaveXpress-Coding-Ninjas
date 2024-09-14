cecf732cf44cb166b886b68dd409b672e982d868
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-14 13:16:25 +0530

    adding updateLeaveById mapping in Leave controller

--- src/main/java/com/CodingNinjas/LeaveXpress/controller/LeaveController.java
+++ src/main/java/com/CodingNinjas/LeaveXpress/controller/LeaveController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.CodingNinjas.LeaveXpress.dto.LeaveDto;
 import com.CodingNinjas.LeaveXpress.model.LeaveModel;
 import com.CodingNinjas.LeaveXpress.service.LeaveService;
 
@@ -49,5 +52,10 @@ public class LeaveController {
     public boolean getLeaveStatusById(Long id) {
         return leaveService.getLeaveStatusById(id);
     }
-    
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateLeaveById(@PathVariable Long id, @RequestBody LeaveDto leaveDto){
+        leaveService.updateLeaveById(id,leaveDto);
+    }
 }
