08aad9cebb2b9ee214033294170519d13a6d3bfa
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-14 12:15:42 +0530

    adding getAllLeave mapping in Leave controller

--- src/main/java/com/CodingNinjas/LeaveXpress/controller/LeaveController.java
+++ src/main/java/com/CodingNinjas/LeaveXpress/controller/LeaveController.java
@@ -1,5 +1,7 @@
 package com.CodingNinjas.LeaveXpress.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.CodingNinjas.LeaveXpress.model.LeaveModel;
 import com.CodingNinjas.LeaveXpress.service.LeaveService;
+
 
 @RestController
 @RequestMapping("/api/leave")
@@ -23,4 +26,11 @@ public class LeaveController {
     public LeaveModel getLeaveById(@PathVariable Long id){
         return leaveService.getLeaveById(id);
     }
+
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<LeaveModel> getAllLeave(){
+        return leaveService.getAllLeaves();
+    }
+    
 }
