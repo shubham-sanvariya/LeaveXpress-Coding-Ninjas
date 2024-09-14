86397450d6822f02b2e8f2ab0fe42f3069986103
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-14 11:42:09 +0530

    adding Leave Service

---
+++ src/main/java/com/CodingNinjas/LeaveXpress/service/LeaveService.java
@@ -0,0 +1,13 @@
+package com.CodingNinjas.LeaveXpress.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.CodingNinjas.LeaveXpress.exception.LeaveNotFoundException;
+import com.CodingNinjas.LeaveXpress.model.LeaveModel;
+import com.CodingNinjas.LeaveXpress.repository.LeaveRepository;
+
+@Service
+public class LeaveService {
+    
+}
