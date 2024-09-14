588a97a50f3c68f9bd2876e34c770fb92e9e0750
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-14 12:06:34 +0530

    adding Leave Controller

---
+++ src/main/java/com/CodingNinjas/LeaveXpress/controller/LeaveController.java
@@ -0,0 +1,10 @@
+package com.CodingNinjas.LeaveXpress.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/leave")
+public class LeaveController {
+    
+}
