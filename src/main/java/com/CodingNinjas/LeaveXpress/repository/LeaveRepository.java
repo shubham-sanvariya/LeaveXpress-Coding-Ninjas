9244d5ee376b3a2511a6e3a1602fd2153962b329
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-14 11:48:57 +0530

    adding findByAccepted method in Leave Repository

--- src/main/java/com/CodingNinjas/LeaveXpress/repository/LeaveRepository.java
+++ src/main/java/com/CodingNinjas/LeaveXpress/repository/LeaveRepository.java
@@ -1,5 +1,7 @@
 package com.CodingNinjas.LeaveXpress.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.CodingNinjas.LeaveXpress.model.LeaveModel;
 @Repository
 public interface LeaveRepository extends JpaRepository<LeaveModel,Long>{
     
+    List<LeaveModel> findByAccepted(boolean accepted);
 }
